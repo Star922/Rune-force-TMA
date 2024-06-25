0d45680ec5228defd1016b2e4eec5c083133360f
Star922 <kyryllnetesa@gmail.com>
2024-06-26 05:35:17 +0900

    update interface

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,132 +1,97 @@
-
-import { useEffect, useState } from 'react';
-import Coin from '/image/coin.svg';
-import { useNavigate } from "react-router-dom";
-import { useGlobalContext } from '../context/GlobalContext';
+import { useEffect, useState } from "react";
+import { useGlobalContext } from "../context/GlobalContext";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { TonConnectButton, useTonAddress } from "@tonconnect/ui-react";
-import axios from 'axios';
-import lock from '/image/lock.svg';
-import spaceShip from '/image/spaceship.svg';
-import { calculateRemainingTime } from '../utils/spaceship';
+import ProgressBar from "../component/ProgressBar";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const { setUser } = useGlobalContext();
   const address = useTonAddress();
 
   const [wallet, setWalletAddress] = useState("");
-  console.log("wallet: ", wallet)
-  const [telUser, setTelUser] = useState<{ username: string; id: string }>({
-    username: "",
-    id: "",
-  });
-  const [accountInfo, setAccountInfo] = useState({
-    coin: 0,
-    lastScore: 0,
-    currentTrial: 3
-  });
+  console.log("wallet: ", wallet);
 
   useEffect(() => {
     const webapp = (window as any).Telegram?.WebApp.initDataUnsafe;
-    console.log("webapp-------- ", webapp)
+    console.log("webapp-------- ", webapp);
     if (webapp && webapp["user"]) {
-      setTelUser({
-        username: webapp["user"]["username"],
-        id: webapp["user"]["id"],
-      });
       setUser({
         username: webapp["user"]["username"],
         id: webapp["user"]["id"],
-      })
+      });
       console.log("=========>webapp", webapp["user"]);
     }
-
   }, []);
-
-  const [chargeHour, setChargeHour] = useState({
-    resMins: "59",
-    resSeconds: "59",
-  })
-  async function fetchData(walletAddress: string) {
-    try {
-      axios.post(`${backendUrl}/user/login`, {
-        wallet: walletAddress,
-        telID: telUser.id,
-        username: telUser.username,
-      }).then(data => {
-        console.log("GetData: ", data.data.data);
-        setAccountInfo(data.data.data);
-        let intervalId;
-        if (data.data.currentTrial === 0) {
-          intervalId = setInterval(() => {
-            const remainingTime = calculateRemainingTime(data.data.ridTime);
-            setChargeHour(remainingTime);
-          }, 1000);
-        } else {
-          clearInterval(intervalId);
-        }
-      })
-    } catch (error) {
-      console.error("There was a problem with the fetch operation:", error);
-      toast.error("Internal Server Error");
-    }
-  }
 
   useEffect(() => {
     if (address) {
       setWalletAddress(address);
-      fetchData(address)
     }
-  }, [address])
+  }, [address]);
 
-  const navigate = useNavigate();
-
-  const handlePlay = () => {
-    if (!address) {
-      toast.error("Please connect your wallet first");
-      return;
-    }
-    navigate('/Tap')
-  }
   return (
-    <div className="w-full h-full flex flex-col py-[5%] px-3 ">
+    <div className="w-full h-screen flex flex-col p-3">
       <ToastContainer />
-      <div className={`ml-auto text-right flex flex-row gap-3 justify-between items-center`}>
-        <div className='flex flex-row gap-5 border-2 border-white bg-black rounded-full px-3 py-1 '>
-          <img src={Coin} alt='Coin'></img>
-          <div>{accountInfo.coin}</div>
-        </div>
-        <TonConnectButton className="absolute left-3" />
+      <div className="relative justify-center items-center w-full h-[50px]">
+        <TonConnectButton className="absolute right-0" />
       </div>
-      <div className='text-center mt-[6%] text-black font-press-start' style={{ WebkitTextStrokeColor: 'white' }}>
-        <div className='text-[32px]' style={{ WebkitTextStrokeWidth: 2 }}>record</div>
-        <div className='text-[84px]' style={{ WebkitTextStrokeWidth: 5 }}>{accountInfo.lastScore}</div>
-      </div>
-      <div className='flex justify-center items-center mt-[0%]'>
-        <img src={spaceShip} alt='space-ship'></img>
-      </div>
-      {
-        accountInfo.currentTrial ?
-          <div className='flex flex-col justify-center items-center mt-[8%] font-press-start'>
-            <div
-              className='text-[28px] text-black bg-[#43E0F7] py-6 px-7 rounded-full blur-outline-all'
-              onClick={() => { handlePlay() }}
-            >
-              Play now
-            </div>
-            <div className='text-[12px] text-black mt-[4vh]'
-              style={{ WebkitTextStrokeColor: 'white', WebkitTextStrokeWidth: 0.5 }}>{accountInfo?.currentTrial} trials available</div>
-          </div> :
+      <div className="flex flex-col h-full relative items-center justify-around">
+        <div
+          className="text-center mt-[10%] text-black font-press-start"
+          style={{ WebkitTextStrokeColor: "white" }}
+        >
           <div
-            className='text-[28px] text-black bg-[#CBEFF9] py-6 px-7 rounded-full blur-outline-all flex flex-row gap-5'
+            className="text-[18px] font-[1000]"
+            style={{ WebkitTextStrokeWidth: 1 }}
           >
-            <img src={lock}></img> New Trials in {chargeHour.resMins + ' : ' + chargeHour.resSeconds}
+            Rune Force Points
           </div>
-      }
+          <div className="text-[64px]" style={{ WebkitTextStrokeWidth: 4 }}>
+            0
+          </div>
+        </div>
+        <div className="flex justify-center items-center relative mt-[0%]">
+          <img
+            src="/image/ellipse.png"
+            alt="coin"
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-auto"
+          ></img>
+          <img
+            src="/image/HealthHero.png"
+            alt="space-ship"
+            className="relative z-10 w-64 h-auto"
+          ></img>
+        </div>
+        <div className="w-full">
+          <div className="flex flex-col items-center not-selectable w-full">
+            <div className="w-full px-10">
+              <div className="flex justify-between items-baseline w-full mb-2">
+                <span className="font-press-start text-[#9E9E9E] text-[10px] font-bold">
+                  Today’s Tap Limit
+                </span>
+                <h3 className="text-500 text-xl font-bold">2000</h3>
+              </div>
+              <ProgressBar value={100} />
+            </div>
+
+            <Link
+              to=""
+              className="flex flex-col items-center mt-3 justify-center cursor-pointer transform origin-bottom transition"
+            >
+              <img
+                src="/image/play.png"
+                alt="play"
+                className="w-12 h-12 p-[1px] rounded-lg"
+              />
+              <p className="text-md font-bold text-[#E5D0B3]">Play</p>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
