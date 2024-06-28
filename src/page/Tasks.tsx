c128e7481379ae12524bc7b4b8b8f4478e837201
Star922 <kyryllnetesa@gmail.com>
2024-06-29 01:11:11 +0900

    update task responsive

--- src/page/Tasks.tsx
+++ src/page/Tasks.tsx
@@ -24,100 +24,115 @@ const Tasks = () => {
         Tasks
       </div>
       <div
-        className="flex flex-row justify-center gap-2 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
+        className="flex flex-row justify-between gap-2 pl-4 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
         onClick={() => {
           setIsModalOpen(true), setFollowType("Twitter");
         }}
       >
-        <div className="flex-none">
-          <img
-            src="/image/Twitter.png"
-            className="w-[24px] h-[24px] rounded-md"
-          ></img>
+        <div className="flex items-center gap-3">
+          <div className="flex-none">
+            <img
+              src="/image/Twitter.png"
+              className="w-[24px] h-[24px] rounded-md"
+            ></img>
+          </div>
+          <div className="px-2 text-left text-black text-[15px]">
+            Follow us on X
+          </div>
         </div>
-        <div className="px-2 text-left text-black text-[15px]">
-          Follow us on X
-        </div>
+
         <div className="bg-black rounded-full justify-between flex flex-row gap-1 px-3 py-1 border-2 border-white items-center">
           <img src={Coin} alt="Coin"></img>
           <div className="text-white pr-3 text-[12px]">+125</div>
         </div>
       </div>
       <div
-        className="flex flex-row justify-center gap-2 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
+        className="flex flex-row justify-between gap-2 pl-4 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
         onClick={() => {
           setIsModalOpen(true), setFollowType("Telegram");
         }}
       >
-        <div className="flex-none">
-          <img
-            src="/image/Telegram.png"
-            className="w-[24px] h-[24px] rounded-md"
-          ></img>
+        <div className="flex items-center gap-3">
+          <div className="flex-none">
+            <img
+              src="/image/Telegram.png"
+              className="w-[24px] h-[24px] rounded-md"
+            ></img>
+          </div>
+          <div className="px-2 text-left text-black text-[15px]">
+            Join our Telegram
+          </div>
         </div>
-        <div className="px-2 text-left text-black text-[15px]">
-          Join our Telegram
-        </div>
+
         <div className="bg-black rounded-full justify-between flex flex-row gap-1 px-3 py-1 border-2 border-white items-center">
           <img src={Coin} alt="Coin"></img>
           <div className="text-white pr-3 text-[12px]">+125</div>
         </div>
       </div>
       <div
-        className="flex flex-row justify-center gap-2 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
+        className="flex flex-row justify-between gap-2 pl-4 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
         onClick={() => {
           setIsModalOpen(true), setFollowType("ETH");
         }}
       >
-        <div className="flex-none">
-          <img
-            src="/image/ETH.png"
-            className="w-[24px] h-[24px] rounded-md"
-          ></img>
+        <div className="flex items-center gap-3">
+          <div className="flex-none">
+            <img
+              src="/image/ETH.png"
+              className="w-[24px] h-[24px] rounded-md"
+            ></img>
+          </div>
+          <div className="px-2 text-left text-black text-[15px]">
+            Share your ETH Wallet
+          </div>
         </div>
-        <div className="px-2 text-left text-black text-[15px]">
-          Share your ETH Wallet
-        </div>
+
         <div className="bg-black rounded-full justify-between flex flex-row gap-1 px-3 py-1 border-2 border-white items-center">
           <img src={Coin} alt="Coin"></img>
           <div className="text-white pr-3 text-[12px]">+125</div>
         </div>
       </div>
       <div
-        className="flex flex-row justify-center gap-2 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
+        className="flex flex-row justify-between gap-2 pl-4 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
         onClick={() => {
           setIsModalOpen(true), setFollowType("Email");
         }}
       >
-        <div className="flex-none">
-          <img
-            src="/image/Email.png"
-            className="w-[24px] h-[24px] rounded-md"
-          ></img>
+        <div className="flex items-center gap-3">
+          <div className="flex-none">
+            <img
+              src="/image/Email.png"
+              className="w-[24px] h-[24px] rounded-md"
+            ></img>
+          </div>
+          <div className="px-2 text-left text-black text-[15px]">
+            Share your email
+          </div>
         </div>
-        <div className="px-2 text-left text-black text-[15px]">
-          Share your email
-        </div>
+
         <div className="bg-black rounded-full justify-between flex flex-row gap-1 px-3 py-1 border-2 border-white items-center">
           <img src={Coin} alt="Coin"></img>
           <div className="text-white pr-3 text-[12px]">+125</div>
         </div>
       </div>
       <div
-        className="flex flex-row justify-center gap-2 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
+        className="flex flex-row justify-between gap-2 pl-4 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
         onClick={() => {
           setIsModalOpen(true), setFollowType("Website");
         }}
       >
-        <div className="flex-none">
-          <img
-            src="/image/Website.png"
-            className="w-[24px] h-[24px] rounded-md"
-          ></img>
+        <div className="flex items-center gap-3">
+          <div className="flex-none">
+            <img
+              src="/image/Website.png"
+              className="w-[24px] h-[24px] rounded-md"
+            ></img>
+          </div>
+          <div className="px-2 text-left text-black text-[15px]">
+            Visit our website
+          </div>
         </div>
-        <div className="px-2 text-left text-black text-[15px]">
-          Visit our website
-        </div>
+
         <div className="bg-black rounded-full justify-between flex flex-row gap-1 px-3 py-1 border-2 border-white items-center">
           <img src={Coin} alt="Coin"></img>
           <div className="text-white pr-3 text-[12px]">+125</div>
