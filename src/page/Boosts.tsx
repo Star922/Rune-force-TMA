cc79708886f159484ebb3d7d9966679d12f0afe9
Star922 <kyryllnetesa@gmail.com>
2024-07-04 05:37:31 +0900

    update modal

--- src/page/Boosts.tsx
+++ src/page/Boosts.tsx
@@ -1,22 +1,68 @@
 import { useState } from "react";
 import Coin from "/image/coin.svg";
-import TermsModal from "../component/FollowModal";
+import BoostModal from "../component/BoostModal";
 import Footer from "../component/Footer";
 
 const Boosts = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [followType, setFollowType] = useState("Twitter");
+  const [followType, setFollowType] = useState("energyLimit");
   const totalScore = localStorage.getItem("total");
 
   const [energyLimit, setEnergyLimit] = useState(1);
   const [rechargingSpeed, setRechargingSpeed] = useState(1);
   const [multiCoins, setMultiCoins] = useState(1);
 
+  const handleClick = (type: string) => {
+    if (type === "energyLimit") {
+      setFollowType("energyLimit");
+      setEnergyLimit(energyLimit + 1);
+      setIsModalOpen(true);
+    } else if (type === "rechargingSpeed") {
+      setFollowType("rechargingSpeed");
+      setRechargingSpeed(rechargingSpeed + 1);
+      setIsModalOpen(true);
+    } else if (type === "multiCoins") {
+      setFollowType("multiCoins");
+      setMultiCoins(multiCoins + 1);
+      setIsModalOpen(true);
+    }
+  };
+
+  const getFollowText = (followType: string) => {
+    if (followType === "energyLimit") {
+      return "Energy Limit";
+    } else if (followType === "rechargingSpeed") {
+      return "Recharging Speed";
+    } else if (followType === "multiCoins") {
+      return "Multi Coins";
+    }
+  };
+
+  const getFollowLevel = (followType: string) => {
+    if (followType === "energyLimit") {
+      return energyLimit + 1;
+    } else if (followType === "rechargingSpeed") {
+      return rechargingSpeed + 1;
+    } else if (followType === "multiCoins") {
+      return multiCoins + 1;
+    }
+  };
+
+  const getFollowSrc = (followType: string) => {
+    if (followType === "energyLimit") {
+      return "/image/energy-limit.png";
+    } else if (followType === "rechargingSpeed") {
+      return "/image/recharging-speed.png";
+    } else if (followType === "multiCoins") {
+      return "/image/multi-coin.png";
+    }
+  };
+
   return (
     <div className="w-full h-full flex flex-col py-3 px-3 justify-between">
       <div className="bg-black rounded-full ml-auto text-right flex flex-row gap-3 justify-between px-3 py-1 border-2 border-white items-center">
         <img src={Coin} alt="Coin"></img>
-        <div>{totalScore}</div>
+        <div>{totalScore || 0}</div>
       </div>
       <div>
         <div
@@ -31,9 +77,7 @@ const Boosts = () => {
         </div>
         <div
           className="flex flex-row justify-between gap-2 pl-4 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
-          onClick={() => {
-            setIsModalOpen(true), setFollowType("Telegram");
-          }}
+          onClick={() => handleClick("energyLimit")}
         >
           <div className="flex items-center gap-3">
             <div className="flex-none">
@@ -57,9 +101,7 @@ const Boosts = () => {
         </div>
         <div
           className="flex flex-row justify-between gap-2 pl-4 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
-          onClick={() => {
-            setIsModalOpen(true), setFollowType("ETH");
-          }}
+          onClick={() => handleClick("rechargingSpeed")}
         >
           <div className="flex items-center gap-3">
             <div className="flex-none">
@@ -85,9 +127,7 @@ const Boosts = () => {
         </div>
         <div
           className="flex flex-row justify-between gap-2 pl-4 items-center py-2 font-press-start border-4 border-[#43E0F7] rounded-xl bg-[#CBEFF9] p-2 mt-3"
-          onClick={() => {
-            setIsModalOpen(true), setFollowType("Email");
-          }}
+          onClick={() => handleClick("multiCoins")}
         >
           <div className="flex items-center gap-3">
             <div className="flex-none">
@@ -114,11 +154,14 @@ const Boosts = () => {
       <Footer />
 
       {isModalOpen && (
-        <TermsModal
+        <BoostModal
           setIsModalOpen={setIsModalOpen}
           followType={followType}
+          text={getFollowText(followType)}
+          level={getFollowLevel(followType)}
+          src={getFollowSrc(followType)}
           isModalOpen={isModalOpen}
-        ></TermsModal>
+        ></BoostModal>
       )}
     </div>
   );
